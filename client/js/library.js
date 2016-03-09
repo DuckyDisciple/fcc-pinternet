cfac9b8ed544db3ceaa5e3cfb22f868e0f883ee8
Drew Sartorius <duckydesign@gmail.com>
2016-03-09 18:16:27 -0500

    Replace dead link

--- client/js/library.js
+++ client/js/library.js
@@ -7,5 +7,9 @@ $(".remove-pin").on('click',function(){
     var delUrl = "/delete/"+ $(this).attr("id");
     $.post(delUrl,function(){
         location.reload();
-    })
-})
+    });
+});
+
+$("img").on('error',function(){
+    $(this).unbind("error").attr("src","/client/img/DeadLink.png");
+});
